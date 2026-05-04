b91eb11a6d5350a1b8f25270fe7093cea8e0ec88
akvdkharnath <akvdkharnath@gmail.com>
2026-05-04 19:54:57 +0530

    Remove Upwanted packages

--- Mini Projects/A5/app/config/db.py
+++
@@ -1,7 +0,0 @@
-from click import echo
-from sqlalchemy import create_engine
-from app.config.config import Config
-
-engine = create_engine(
-    Config.SQLALCHEMY_DATABASE_URL, pool_pre_ping=True
-)
